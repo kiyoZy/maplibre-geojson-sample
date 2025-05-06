db05a3bba27e2ef349b950ba01c1567b292df8a5
seaweed28 <seaweedkids0825@gmail.com>
2025-05-06 19:50:50 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,35 @@
+const geojsonData = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [139.6917, 35.6895]
+            },
+            "properties": {
+                "name": "東京駅"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [139.7671, 35.6812]
+            },
+            "properties": {
+                "name": "渋谷"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [139.7010, 35.6581]
+            },
+            "properties": {
+                "name": "新宿"
+            }
+        }
+    ]
+};
